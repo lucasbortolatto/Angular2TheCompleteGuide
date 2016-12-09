440756f616cd19d08692748d0d8f32caabc22570
Luidson Lucas <lucasbortolatto2@hotmail.com>
2016-12-09 09:39:42 -0200

    Removind othercomponent

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -4,19 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
       <h1>Inline Template</h1>
-      <firstapp-other></firstapp-other>
-      <firstapp-another>
-        <div>
-            <h2>Hello</h2>
-            <p>World</p>
-        </div>
-      </firstapp-another>
-  `,
-  styles: [`
-    h1 {
-        color: red;
-    }
-    `]
+  `
 })
 export class AppComponent {
   title = 'app works!';
